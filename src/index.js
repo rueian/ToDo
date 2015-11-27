37615dca595ff19ff48088acc470d5d7430a0ad4
Rueian Oneecom <rueiancsie@gmail.com>
2015-11-27 15:04:26 +0800

    Add Parse login

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Parse from 'parse';
 import { FB_APP_ID, FB_VERSION, PARSE_ID, PARSE_KEY } from '../config';
 // import { App } from './App';
 
-window.fbAsyncInit = function() {
-  FB.init({
-    appId: FB_APP_ID,
-    cookie: true,
-    xfbml: true,
-    version: FB_VERSION
-  });
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-};
-
-// render(<App />, document.getElementById('root'));
-//
-// console.log(FB_APP_ID)
-
 function statusChangeCallback(response) {
-  console.log('statusChangeCallback');
-  console.log(response);
-
+  console.log('statusChangeCallback', response);
   if (response.status === 'connected') {
-    document.getElementById('welcome').classList.add("hidden");
-    document.getElementById('content').classList.remove("hidden");
-    // render(<App />, document.getElementById('root'));
-    testAPI();
+    parseLogin();
   } else {
-    document.getElementById('welcome').classList.remove("hidden");
-    document.getElementById('loading').classList.add("hidden");
-    document.getElementById('login').classList.remove("hidden");
-    document.getElementById('content').classList.add("hidden");
+    showWelcome();
   }
 }
 
@@ -49,12 +25,54 @@ function testAPI() {
 function doFBLogin() {
   FB.getLoginStatus(function(response) {
     if (response == 'connected') return;
-
-    FB.login(function(res) {
-      statusChangeCallback(res);
-    }, {scope: 'public_profile,email,user_friends'});
+    parseLogin();
   });
 }
+
+function parseLogin() {
+  Parse.FacebookUtils.logIn('public_profile,email,user_friends', {
+    success: function(user) {
+      showApp(user);
+    },
+    error: function(user, error) {
+      alert("User cancelled the Facebook login or did not fully authorize.");
+    }
+  });
+}
+
+function showWelcome() {
+  document.getElementById('welcome').classList.remove("hidden");
+  document.getElementById('loading').classList.add("hidden");
+  document.getElementById('login').classList.remove("hidden");
+  document.getElementById('content').classList.add("hidden");
+}
+
+function showApp(user) {
+  console.log("parse", user);
+  document.getElementById('welcome').classList.add("hidden");
+  document.getElementById('content').classList.remove("hidden");
+  testAPI();
+  // render(<App />, document.getElementById('root'));
+}
+
+Parse.initialize(PARSE_ID, PARSE_KEY);
+
+window.fbAsyncInit = function() {
+  Parse.FacebookUtils.init({
+    appId: FB_APP_ID,
+    cookie: true,
+    xfbml: true,
+    version: FB_VERSION
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("fb_login_btn").addEventListener("click", doFBLogin);
+});
 
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -63,7 +81,3 @@ function doFBLogin() {
   js.src = "//connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById("fb_login_btn").addEventListener("click", doFBLogin);
-});
