e2f96b95d25384cd326b85702af550d754a13579
Rueian Oneecom <rueiancsie@gmail.com>
2015-11-28 14:24:12 +0800

    Add ToDo List

--- src/App.js
+++ src/App.js
@@ -12,9 +12,12 @@ import Avatar from 'material-ui/lib/avatar';
 import Dialog from 'material-ui/lib/dialog';
 import TextField from 'material-ui/lib/text-field';
 import SelectField from 'material-ui/lib/select-field';
+import Snackbar from 'material-ui/lib/snackbar';
 import { TaskList } from './TaskList';
+import Parse from 'parse';
 
 const SelectableList = SelectableContainerEnhance(List);
+const Todo = Parse.Object.extend("Todo");
 
 export class App extends Component {
 
@@ -22,14 +25,40 @@ export class App extends Component {
     super(props);
     this.state = {
       title: '待辦事項',
-      selectValue: this.props.user.get('authData').facebook.userId,
+      isLoading: true,
+      selectValue: props.user.get('authData').facebook.id,
       selectedNav: 1,
+      titleError: '',
       showDialog: false,
       user: {
-        id: this.props.user.get('authData').facebook.userId
-      }
+        id: props.user.get('authData').facebook.id
+      },
+      todos: []
     };
+  }
+
+  componentDidMount() {
     this._getFB();
+    this._getToDos();
+  }
+
+  _getToDos(isDone) {
+    this.setState({isLoading: true});
+
+    let query = new Parse.Query(Todo);
+    query.equalTo('userId', this.state.user.id);
+    if (isDone) {
+      query.equalTo('isDone', true);
+    } else {
+      query.notEqualTo('isDone', true);
+    }
+    query.descending('createdAt');
+    console.log('query',query,this.state.user.id);
+    query.find().then((todos) => {
+      this.setState({todos: todos, isLoading: false});
+    }, (err) => {
+      console.error(err);
+    });
   }
 
   _getFB() {
@@ -54,32 +83,52 @@ export class App extends Component {
     this.refs.leftNav.toggle();
   }
 
-  _getToDo() {
-
-  }
-
-  _getArchived() {
-
-  }
-
   _newToDo() {
+    const snackbar = this.refs.snackbar;
 
+    let title = this.refs.input.getValue();
+    let userId = this.state.user.id;
+    let creatorId = this.state.selectValue || userId;
+
+    if (!title) {
+      return this.setState({titleError: '請輸入內容'});
+    } else {
+      this.setState({titleError: ''});
+    }
+
+    this.setState({showDialog: false});
+
+    let todo = new Todo();
+
+    todo.save({
+      title,
+      userId,
+      creatorId
+    }).then((todo) => {
+      snackbar.show();
+      this._getToDos();
+    }, (err) => {
+      console.error(err);
+    });
   }
 
   _openModal(e) {
-    this.setState({showDialog: true});
+    this.setState({showDialog: true}, () => {
+      this.refs.input.focus();
+    });
   }
 
   _closeModal(e) {
     this.setState({showDialog: false});
   }
 
-  _handleInputTitleChange(e) {
-
-  }
-
   _handleSelectValueChange(value) {
     this.setState({selectValue: value});
+  }
+
+  _handleSnackbarCancel() {
+    const snackbar = this.refs.snackbar;
+    snackbar.dismiss();
   }
 
   render() {
@@ -97,24 +146,23 @@ export class App extends Component {
     return (
       <div>
         <AppBar title={this.state.title} showMenuIconButton={true} onLeftIconButtonTouchTap={this._openMenu.bind(this)}/>
-        <div style={{padding: 24, maxWidth: 800, marginLeft: 'auto', marginRight: 'auto'}}>
+        <div style={{padding: 12, maxWidth: 800, marginLeft: 'auto', marginRight: 'auto'}}>
           <Paper zDepth={1}>
-            <TaskList />
+            <TaskList todos={this.state.todos}/>
           </Paper>
         </div>
         <FloatingActionButton style={{position: 'fixed', right: 20, bottom: 20}} onTouchTap={this._openModal.bind(this)}>
           <FontIcon className="material-icons">add</FontIcon>
         </FloatingActionButton>
-        <Dialog
+        <Dialog ref="modal"
           title="創建 ToDo"
           actions={modalAction}
-          actionFocus="submit"
           open={this.state.showDialog}
           onRequestClose={this._closeModal}>
-          <TextField
+          <TextField ref="input"
+            errorText={this.state.titleError}
             hintText="12/4 要交 Web App 作業"
-            floatingLabelText="輸入待辦事項內容"
-            onChange={this._handleInputTitleChange.bind(this)} />
+            floatingLabelText="輸入待辦事項內容" />
           <SelectField
             valueLink={{value: this.state.user.id, requestChange: this._handleSelectValueChange.bind(this)}}
             floatingLabelText="選擇指派對象"
@@ -122,6 +170,11 @@ export class App extends Component {
             displayMember="name"
             menuItems={users} />
         </Dialog>
+        <Snackbar ref="snackbar"
+          message="ToDo 創建成功"
+          action="取消"
+          autoHideDuration={1000}
+          onActionTouchTap={this._handleSnackbarCancel.bind(this)}/>
         <LeftNav ref="leftNav" docked={false} selectedIndex={1}
           header={<ListItem primaryText={this.state.user.name} leftAvatar={<Avatar src={userPhoto} />} />}>
           <SelectableList
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
+import Avatar from 'material-ui/lib/avatar';
+import Checkbox from 'material-ui/lib/checkbox';
 
 export class TaskList extends Component {
   render() {
+    let todos = this.props.todos.map((todo) => {
+      let userPhoto = '//graph.facebook.com/v2.5/' + todo.get('creatorId') + '/picture?type=large';
+      return (
+        <ListItem
+          primaryText={todo.get('title')}
+          leftCheckbox={<Checkbox defaultChecked={todo.get('isDone')}/>}
+          rightAvatar={<Avatar src={userPhoto} />}/>
+      )
+    });
     return (
       <List>
+        {todos}
       </List>
     );
   }
