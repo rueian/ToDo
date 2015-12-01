f7324d23a3526dec3836ec4b168a26bdf74755d6
Rueian Oneecom <rueiancsie@gmail.com>
2015-12-01 22:57:20 +0800

    Add empty hint

--- src/TaskList.js
+++ src/TaskList.js
@@ -6,23 +6,29 @@ import Avatar from 'material-ui/lib/avatar';
 import Checkbox from 'material-ui/lib/checkbox';
 
 export class TaskList extends Component {
+  _buildListItem(todo, index) {
+    let avatar;
+    if (this.props.user.id == todo.get('creatorId')) {
+      avatar = <Avatar src={avatarPath(todo.get('userId'))} />;
+    } else {
+      avatar = <Avatar src={avatarPath(todo.get('creatorId'))} />;
+    }
+    return (
+      <ListItem
+        style={{textDecoration: todo.get('isDone') ? 'line-through' : 'initial'}}
+        key={todo.id}
+        primaryText={todo.get('title')}
+        leftCheckbox={<Checkbox defaultChecked={todo.get('isDone')} onCheck={this.props.handleToDoClick.bind(null, index)}/>}
+        rightAvatar={avatar}/>
+    )
+  }
+
   render() {
-    let todos = this.props.todos.map((todo, index) => {
-      let avatar;
-      if (this.props.user.id == todo.get('creatorId')) {
-        avatar = <Avatar src={avatarPath(todo.get('userId'))} />;
-      } else {
-        avatar = <Avatar src={avatarPath(todo.get('creatorId'))} />;
-      }
-      return (
-        <ListItem
-          style={{textDecoration: todo.get('isDone') ? 'line-through' : 'initial'}}
-          key={todo.id}
-          primaryText={todo.get('title')}
-          leftCheckbox={<Checkbox defaultChecked={todo.get('isDone')} onCheck={this.props.handleToDoClick.bind(null, index)}/>}
-          rightAvatar={avatar}/>
-      )
-    });
-    return (<List>{todos}</List>);
+    if (this.props.todos.length) {
+      let todos = this.props.todos.map((todo, index) => this._buildListItem(todo, index));
+      return (<List>{todos}</List>)
+    } else {
+      return (<div className="empty-hint">指派事項 <i className="fa fa-hand-o-right"></i></div>)
+    }
   }
 }
